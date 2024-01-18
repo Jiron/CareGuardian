dd6dfe3c594cc84f814777f759054175e3cec4fd
Manassés Zähnler <manasses.zaehnler@opten.ch>
2024-01-18 16:41:43 +0100

    feat: added design for countdown

--- app/src/main/java/com/example/careguardian/CountdownActivity.java
+++ app/src/main/java/com/example/careguardian/CountdownActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CountdownActivity extends AppCompatActivity {
 
     private boolean countDownRanOut = false;
     private int countDownSeconds = 10;
     private TextView countDownText;
+    private TextView countDownTextDetail;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +25,26 @@ public class CountdownActivity extends AppCompatActivity {
         setContentView(R.layout.activity_countdown);
 
         countDownText = findViewById(R.id.countDownText);
+        countDownTextDetail = findViewById(R.id.countDownTextDetail);
+        backButton = findViewById(R.id.backButton);
 
         refreshCountdown();
+
+        backButton.setOnClickListener(view -> finish());
     }
 
     private void refreshCountdown() {
         if(countDownSeconds >= 0) {
             new Handler().postDelayed(() -> {
-                countDownText.setText(countDownSeconds + " Seconds left");
+                countDownText.setText(countDownSeconds + " Seconds");
                 countDownSeconds--;
                 refreshCountdown();
             }, 1000);
         } else {
             countDownRanOut = true;
-            countDownText.setText("Help is on the way!");
+            countDownText.setText("Emergency contacts\nhave been notified");
+            countDownTextDetail.setText("");
+            backButton.setText("Back to Home");
         }
     }
 }
