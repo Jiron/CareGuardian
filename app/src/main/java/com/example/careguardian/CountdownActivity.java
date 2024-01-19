2ac13f7510987339de5d4716bc84d862292525cb
Manassés Zähnler <manasses.zaehnler@opten.ch>
2024-01-19 09:36:53 +0100

    feat: add vibration

--- app/src/main/java/com/example/careguardian/CountdownActivity.java
+++ app/src/main/java/com/example/careguardian/CountdownActivity.java
@@ -3,8 +3,13 @@ package com.example.careguardian;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -39,12 +44,24 @@ public class CountdownActivity extends AppCompatActivity {
                 countDownText.setText(countDownSeconds + " Seconds");
                 countDownSeconds--;
                 refreshCountdown();
+
+                vibrate(new long[] { 100, 100, 100, 200 }, new int[] { 100, 150, 0, 255 }, -1); // transition up, short pause, strong vibration, 500ms total
             }, 1000);
         } else {
             countDownRanOut = true;
             countDownText.setText("Emergency contacts\nhave been notified");
             countDownTextDetail.setText("");
             backButton.setText("Back to Home");
+        }
+    }
+
+    private void vibrate(long[] timings, int[] amplitudes, int repeatIndex) {
+        Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createWaveform(timings, amplitudes, repeatIndex));
+        } else {
+            v.vibrate(500);
         }
     }
 }
